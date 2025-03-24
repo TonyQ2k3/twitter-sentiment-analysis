47578bb4b39a98ee628d692fade08b3336720b46
TonyQ2k3 <ntbquoc2015@gmail.com>
2025-03-24 21:11:59 +0700

    First commit

---
+++ app/test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+df = pd.read_csv('data/iphone_15_tweets_brief.csv')
+print(df.columns)
---
+++ kubernetes/setup.sh
@@ -0,0 +1,5 @@
+# Apply all Kubernetes resources in the current directory
+
+#!/bin/bash
+
+kubectl apply -f .
